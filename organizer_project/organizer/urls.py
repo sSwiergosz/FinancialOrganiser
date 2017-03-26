4f7b603e425edfffdebac5a1f13761b544ca4b3c
sSwiergosz <szymon.swiergosz@gmail.com>
2017-03-27 00:15:07 +0200

    Login and new background added.

--- organizer_project/organizer/urls.py
+++ organizer_project/organizer/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'organizer'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^login/$', auth_views.login, {'template_name': 'organizer/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^home/$', views.home, name='home'),
 ]
--- organizer_project/organizer/views.py
+++ organizer_project/organizer/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
 	
 	return render(request, 'organizer/index.html')
+
+
+def home(request):
+
+	return render(request, 'organizer/home.html')
